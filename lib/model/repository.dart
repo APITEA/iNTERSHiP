4c39ed177e28dd8c1c0152c86c4747c4501bcdb2
pavelkafil <pavelkafil@email.cz>
2019-05-21 12:18:04 +0200

    Test commit and permission to repository

--- lib/model/repository.dart
+++ lib/model/repository.dart
@@ -4,9 +4,9 @@ import 'application.dart';
 class ApplicationRepository {
 
   List<Application> getApplications() {
-    var app1 = Application(name: "Ab1", url: "URL");
-    var app2 = Application(name: "Ab1", url: "URL");
-    var app3 = Application(name: "Ab1", url: "URL");
+    var app1 = Application(name: "App 1", url: "http://example1.com");
+    var app2 = Application(name: "App 2", url: "http://example2.com");
+    var app3 = Application(name: "App 3", url: "http://example3.com");
     return [app1, app2, app3];
   }
 }
