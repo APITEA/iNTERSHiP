ace6329167d1cb71c8139e62b0ca893f3925a556
pavelkafil <pavelkafil@email.cz>
2019-05-22 12:08:53 +0200

    Upravy aplikace

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,90 @@
 import "package:flutter/material.dart";
 
+import 'model/application.dart';
 import 'model/repository.dart';
 
-void main(){
-
+void main() {
   var applicationRepository = ApplicationRepository();
 
-  runApp(MaterialApp(
-    title: "Spikey",
-    home: Scaffold(
-      appBar: AppBar(
-          title: Text("Spikey"),
-          backgroundColor: Colors.green,
-      ),
-      body: getListView(applicationRepository),
+  runApp(
+    MaterialApp(
+      title: "Spikey",
+      home: Scaffold(
+          appBar: AppBar(
+            title: new Center(child: new Text("Spikey", textAlign: TextAlign.center)),
+            backgroundColor: Colors.green,
+          ),
+          body: getListView(applicationRepository)),
     ),
-  ));
+  );
 }
-
-
 
 //class MyApp extends StatelessWidget {
 
-  //@override
-  //Widget build(BuildContext context) {
-    //return MaterialApp(
-      //title: "Spikey App",
-      //theme: ThemeData(
-        //primarySwatch: Colors.green
-      //),
-      //home: Scaffold(
-        //body: getListView(),
-      //),
-    //);
-  //}
+//@override
+//Widget build(BuildContext context) {
+//return MaterialApp(
+//title: "Spikey App",
+//theme: ThemeData(
+//primarySwatch: Colors.green
+//),
+//home: Scaffold(
+//body: getListView(),
+//),
+//);
+//}
 //}
 
 Widget getListView(ApplicationRepository applicationRepository) {
-
   var applications = applicationRepository
       .getApplications()
-      .map((app) =>
-      ListTile(
-          leading: (Image.network(app.pic)),
-          title: Text(app.name),
-          subtitle: Text(app.url),
-          trailing: Icon(Icons.arrow_forward_ios),
-      ));
+      .map((app) => ListItemView(app: app));
 
   var listView = ListView(children: applications.toList());
 
   return listView;
 }
 
+class ListItemView extends StatelessWidget {
+  final Application app;
 
+  const ListItemView({this.app});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        contentPadding: EdgeInsets.all(3) ,
+        leading: (Image.network(app.pic)),
+        title: Text(app.name),
+        subtitle: Text(app.url),
+        trailing: IconButton(
+            icon: Icon(Icons.arrow_forward_ios),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DetailView(app: this.app,)),
+              );
+            }));
+  }
+}
+
+class DetailView extends StatelessWidget {
+
+  final Application app;
+
+  DetailView({this.app});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: new Center(child: new Text(app.name)),
+        backgroundColor: Colors.green,
+      ),
+    );
+  }
+}
 
 //  //@override
 //  //Widget build(BuildContext context) {
@@ -64,4 +95,3 @@ Widget getListView(ApplicationRepository applicationRepository) {
 //      ),
 //    );
 //  }
-
